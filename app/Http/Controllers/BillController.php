ac94ac3714fcf5fe4b90281fbfc2b8cf0f28766e
MaicolDdox <maicolduvangascarodas@gmail.com>
2025-11-18 09:13:31 -0500

    implementacion de logica en el show

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -3,25 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bill;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class BillController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -34,26 +20,12 @@ class BillController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Bill $bill)
+    public function show(Bill $bill, User $user)
     {
-        //
+        
+        return response()->json($bill, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Bill $bill)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Bill $bill)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
